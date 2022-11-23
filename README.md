f0596e66592107eca5c5b40983c5d2b236679fdb
Brian Zou <brianzou03@gmail.com>
2022-11-23 12:17:55 -0500

    Begin project structuring

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # NLP-copilot
+
+
+## Installations
+```
+pip install autocorrect
+```
+
+## Libraries
+Python autocorrect
+* https://github.com/filyp/autocorrect
+
+## Project Architecture
+
+
+## Future Expansion
+
+
+## Tech Stack
+
+
+## Contact Us
---
+++ main.py
@@ -0,0 +1,21 @@
+from autocorrect import Speller
+
+""" Notes:
+
+References:
+Grammarly
+
+Python Libraries we want to use:
+NLTK Package
+PyDictionary
+
+Goals:
+1. Start with: Grammar Check
+2. Essay Copilot (think github copilot),  correct grammar mistakes, recommend synonyms
+3. Publish a Google plug-in
+"""
+
+
+spell = Speller()  # create Speller object
+
+print(spell("I'm not sleapy and tehre is no place I'm giong to."))
