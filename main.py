eb16236f279cf1110813de451e055dc07f1a375c
Brian Zou <brianzou03@gmail.com>
2022-11-23 15:26:17 -0500

    Add matches, implement methods

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import language_tool_python
 
-
 """ Notes:
 
 References:
@@ -13,18 +12,31 @@ PyDictionary
 Goals:
 1. Start with: Grammar Check
 2. Essay Copilot (think github copilot),  correct grammar mistakes, recommend synonyms
-3. Publish a Google plug-in
+3. Have a switch to swap between suggesting mode and autocomplete/autocorrect mode
+4. Publish a Google plug-in
 """
 
-# spell = Speller()  # create Speller object
-my_tool = language_tool_python.LanguageTool('en-US')  # create language-tool
 
+# return corrected text - spelling + grammar fixed
+def auto_correction(tool, text):
+    correct_text = tool.correct(text)
+
+    return correct_text
+
+
+# get matches to text - used for suggested changes
+def get_matches(tool, text):
+    my_matches = tool.check(my_text)
+    return my_matches
+
+
+my_tool = language_tool_python.LanguageTool('en-US')  # create language-tool
 my_text = "A quick broun fox jumpps over a a little lazy dog. I'm not sleapy and tehre is no place I'm giong to."
 
+print(auto_correction(my_tool, my_text))
 
-# apply correction
-correct_text = my_tool.correct(my_text)
+my_list = get_matches(my_tool, my_text)
 
-# Print text
-print("Original Text: ", my_text)
-print("Text after correction: ", correct_text)
+for match in my_list:
+    print(match)
+
