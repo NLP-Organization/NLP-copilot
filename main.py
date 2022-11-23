fb4e57f3cc8869959d726c148178d820b796da7d
bryantto08 <bryantto08@gmail.com>
2022-11-23 16:10:46 -0500

    tkinter basic gui setup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import language_tool_python
+from tkinter import *
+from tkinter.ttk import *
 
 """ Notes:
 
@@ -20,7 +22,6 @@ Goals:
 # return corrected text - spelling + grammar fixed
 def auto_correction(tool, text):
     correct_text = tool.correct(text)
-
     return correct_text
 
 
@@ -32,6 +33,47 @@ def get_matches(tool, text):
 
 my_tool = language_tool_python.LanguageTool('en-US')  # create language-tool
 my_text = "A quick broun fox jumpps over a a little lazy dog. I'm not sleapy and tehre is no place I'm giong to."
+# create root window
+root = Tk()
+
+# root window title and dimension
+root.title("Welcome to GeekForGeeks")
+# Set geometry(widthxheight)
+root.geometry('700x350')
+
+
+# adding menu bar in root window
+# new item in menu bar labelled as 'New'
+# adding more items in the menu bar
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label='New')
+menu.add_cascade(label='File', menu=item)
+root.config(menu=menu)
+
+# adding a label to the root window
+lbl = Label(root, text="Input sentence: ")
+lbl.grid()
+
+# adding Entry Field
+txt = Entry(root, width=10)
+txt.grid(column=1, row=0)
+
+
+# function to display user text when
+# button is clicked
+def clicked():
+    fix = "Output: " + auto_correction(my_tool, txt.get())
+    lbl.configure(text=fix)
+
+
+# button widget with red color text inside
+btn = Button(root, text="Enter", command=clicked)
+# Set Button Grid
+btn.grid(column=2, row=0)
+
+# Execute Tkinter
+root.mainloop()
 
 print(auto_correction(my_tool, my_text))
 
