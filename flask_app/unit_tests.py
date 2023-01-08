eb91ea27d37c226302e320fdb642ffbd72e8f575
bryantto08 <bryantto08@gmail.com>
2023-01-08 13:24:52 -0500

    simple unit test

---
+++ flask_app/unit_tests.py
@@ -0,0 +1,18 @@
+"""
+Tests for NLP-Copilot
+"""
+from unittest import TestCase, main
+from LanguageHelper import LanguageHelper
+
+class CopilotTestCase(TestCase):
+    
+    def test_auto_correction(self):
+        # Tests functionality of autocorrection
+        my_string = "Thsi is an sentence."
+        lang_help = LanguageHelper()
+
+        self.assertEqual(lang_help.auto_correction(my_string),
+                        "This is a sentence.")
+
+if __name__ == "__main__":
+    main()
